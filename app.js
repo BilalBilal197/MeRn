2cabad2ae6064f61545dbec364d949c6cef1acb1
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-09-02 09:01:00 -0500

    update app file

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express()
 const PORT = 5000
 
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
+app.get('/',(req,res)=>{
+    res.send("hello world")
 })
 
 app.listen(PORT,()=>{
-    console.log("server is running on", PORT)
+    console.log("server is running on",PORT)
 })
