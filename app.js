7cb4a4eeb0d30b531dab4532efe4ed0d892d1ada
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-09-03 08:58:00 -0500

    add customMiddlware

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ const customMiddleware = (req, res, next) => {
     next()
 }
 
-app.use(customMiddleware)
 
 app.get('/',(req,res)=>{
     console.log("home")
     res.send("hello world")
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about',customMiddleware,(req,res)=>{
     console.log("about")
     res.send("about page")
 })
