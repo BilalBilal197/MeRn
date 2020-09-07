58593dc55a2a86970621aea3382b624b4fc7e992
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-09-07 11:02:11 -0500

    add comment line to needless code

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const PORT = 5000
 
 //QwuFsWJfOqiQ3uAN
 
-const customMiddleware = (req, res, next) => {
-    console.log("middleware executed!")
-    next()
-}
+// const customMiddleware = (req, res, next) => {
+//     console.log("middleware executed!")
+//     next()
+// }
 
 
-app.get('/',(req,res)=>{
-    console.log("home")
-    res.send("hello world")
-})
+// app.get('/',(req,res)=>{
+//     console.log("home")
+//     res.send("hello world")
+// })
 
-app.get('/about',customMiddleware,(req,res)=>{
-    console.log("about")
-    res.send("about page")
-})
+// app.get('/about',customMiddleware,(req,res)=>{
+//     console.log("about")
+//     res.send("about page")
+// })
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
