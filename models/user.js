319e6ec01f3cce0badc345db5e5c3290ebba4e06
Bilal Abdullah <bilalxabdullah@gmail.com>
2020-10-02 16:15:02 -0500

    add models

--- models/user.js
+++ models/user.js
@@ -4,5 +4,15 @@ const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    email: {
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
     }
 })
+
+mongoose.model("User", userSchema)
